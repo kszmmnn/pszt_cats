0d78ae67a97352c53f9692b1a5c9501b3e2a14a5
Szymon Kubiszewski <kubiszewskiszymon@gmail.com>
2021-01-25 23:11:57 +0100

    added input standarization

--- src/main.cpp
+++ src/main.cpp
@@ -4,35 +4,60 @@
 
 int main()
 {
-    FileReader* fileReader = FileReader::GetInstance("cats.csv");
-    
+    FileReader *fileReader = FileReader::GetInstance("cats.csv");
+
     int repeat = 10;
     int epochs = 10;
 
-    Network* network;
+    Network *network;
 
     int sumSuccess = 0;
     int successCount = 0;
     int tries;
 
+    std::tuple<bool, double, double> config;
+
+    double bwtMean { 0.0 };
+    double hwtMean { 0.0 };
+    double bwtStdError { 0.0 };
+    double hwtStdError { 0.0 };
+    unsigned populationCount { 0 };
+    fileReader->seekToStart();
+    while (fileReader->ParseConfig(config))
+    {
+        bwtMean += std::get<1>(config);
+        hwtMean += std::get<2>(config);
+        populationCount++;
+    }
+    bwtMean /= populationCount;
+    hwtMean /= populationCount;
+
+    fileReader->seekToStart();
+    while (fileReader->ParseConfig(config))
+    {
+        bwtStdError += std::pow(std::get<1>(config) - bwtMean, 2);
+        hwtStdError += std::pow(std::get<2>(config) - hwtMean, 2);
+    }
+    bwtStdError /= populationCount;
+    hwtStdError /= populationCount;
+    bwtStdError = std::sqrt(bwtStdError);
+    hwtStdError = std::sqrt(hwtStdError);
+
     for (int r = 0; r < repeat; r++)
     {
         network = new Network(2, { 40 }, 2);
         network->SetLearningRate(0.05);
-        
 
-        std::tuple<bool, double, double> config;
         double bwt;
         double hwt;
-
 
         for (int i = 0; i < epochs; i++)
         {
             fileReader->seekToStart();
             while (fileReader->ParseConfig(config))
             {
-                bwt = std::get<1>(config);
-                hwt = std::get<2>(config);
+                bwt = (std::get<1>(config) - bwtMean)/bwtStdError;
+                hwt = (std::get<2>(config) - hwtMean)/hwtStdError;
                 std::vector<double> out;
                 bool isFemale = std::get<0>(config);
 
@@ -45,7 +70,7 @@ int main()
                     out = { 0, 1 };
                 }
 
-                network->SetDataRow({ bwt, hwt }, out);
+                network->SetDataRow( { bwt, hwt }, out);
                 network->Activate();
 
                 network->Backpropagation();
@@ -73,7 +98,7 @@ int main()
                 out = { 0, 1 };
             }
 
-            network->SetDataRow({ bwt, hwt }, out);
+            network->SetDataRow( { bwt, hwt }, out);
             network->Activate();
 
             if (isFemale == network->IsFemale())
@@ -88,8 +113,8 @@ int main()
         sumSuccess += successCount;
     }
     sumSuccess /= repeat;
-    
-    std::cout << sumSuccess << "," << (double)sumSuccess / 144 << std::endl;
-   
-	return 0;
+
+    std::cout << sumSuccess << "," << (double) sumSuccess / 144 << std::endl;
+
+    return 0;
 }
